126c92ff38123d073999d5931c7d7e6f5803480b
Pawel Dabrowa <paweldabrowa@pawels-air.chwv.local>
2019-04-30 16:26:14 +0100

    adding remove elements functions

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
 
-const newElement = document.querySelector('.add-odd-element')
+const addOdd = document.querySelector('.add-odd-element');
+const addEven = document.querySelector('.add-even-element');
+const removeOdd = document.querySelector('.remove-odd-element');
+const removeEven = document.querySelector('.remove-even-element');
+//Adding odd element
+let numberOdd = 1;
 
-let numberEven = 1;
-
-function addElement() {
+function addOddElement() {
     const liElement = document.createElement('li');
-    liElement.textContent = numberEven;
-    numberEven = numberEven + 2;
+    liElement.textContent = numberOdd;
+    numberOdd = numberOdd + 2;
     document.querySelector('ul').appendChild(liElement);
 
-    if ((numberEven) % 3 == 0) {
+    if ((numberOdd) % 3 == 0) {
         liElement.classList.add('big');
     }
 }
 
-newElement.addEventListener('click', addElement);
+removeOddElement = () => {
+    const parentElement = document.querySelector('li');
+    const childElement = document.querySelector('li:nth-last-child(1)');
+    // removeA.remove()
+    return parentElement.parentNode.removeChild(childElement);
+}
+removeOdd.addEventListener('click', removeOddElement);
+
+
+addOdd.addEventListener('click', addOddElement);
+
+
+
+//Adding even element
+let numberEven = 2;
+
+function addEvenElement() {
+    const liElement = document.createElement('li');
+    liElement.textContent = numberEven;
+    numberEven = numberEven + 2;
+    if ((numberEven) % 2 == 1) {
+        numberEven = numberEven + 1;
+    }
+    document.querySelector('ul').appendChild(liElement);
+    if ((numberEven) % 3 == 1) {
+        liElement.classList.add('big');
+    }
+}
+
+addEven.addEventListener('click', addEvenElement);
+
